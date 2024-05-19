70a1633c00cfae73ca5f07ee36938749ebadfb1f
RobsonnTrasel <robson.trasel@gmail.com>
2024-05-18 22:25:58 -0300

    chore: add more custom types for baileys module

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,7 @@
 export type TextFormatter = (s: string) => string;
 export type NormalizedLetterMap = { [key: string]: string };
+export type Mimetype = 'video/mp4' | 'image/gif' | 'image';
+export type Button = {
+    id: string;
+    text: string;
+};
