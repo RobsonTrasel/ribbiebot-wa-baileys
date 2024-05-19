7303518cfea4e4022eb5d3420f2af61c4e5d4312
RobsonnTrasel <robson.trasel@gmail.com>
2024-05-18 22:57:54 -0300

    chore: add new type for mimetype

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type TextFormatter = (s: string) => string;
 export type NormalizedLetterMap = { [key: string]: string };
-export type Mimetype = 'video/mp4' | 'image/gif' | 'image';
+export type Mimetype = 'video/mp4' | 'image/gif' | 'image' | 'application/octet-stream';
 export type Button = {
     id: string;
     text: string;
